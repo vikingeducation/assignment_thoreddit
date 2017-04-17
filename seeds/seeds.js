e65963f60642708fe3585a35601304a6f39acd78
tim5046 <tim5046@gmail.com>
2017-04-17 13:52:07 -0700

    seeds

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,5 +1,6 @@
 var faker = require('faker');
 var voca = require('voca');
+
 const MULTIPLIER = 1;
 
 function randomTitle(type) {
@@ -14,49 +15,49 @@ function randomTitle(type) {
     var index = Math.floor(Math.random() * names.length);
     return names[index];
 }
+
 module.exports = () => {
 
 
 
-
-
-    // ----------------------------------------
-    // Create Users
-    // ----------------------------------------
+// ----------------------------------------
+// Create Users
+// ----------------------------------------
     console.log('Creating Users');
     var users = [];
-    for (let i = 0; i < MULTIPLIER * 2; i++) {
+    for (let i = 1; i < MULTIPLIER * 10; i++) {
         var user = new User({
-            firstName: 'Foo',
-            lastName: 'Bar',
-            username: `foobar${ i }`,
-            email: `foobar${ i }@gmail.com`
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            username: faker.random.word(),
+            email: faker.internet.email()
         });
         users.push(user);
     }
 
-
+    console.log('Creating Posts');
     var posts = [];
-    for (let i = 0; i < MULTIPLIER * 2; i++) {
-        var user = new Post({
-            title: randomTitle('books'),
-            body: randomTitle('books'),
-            author: User.getRandomId();
-        });
-        users.push(user);
+    for (let i = 1; i < MULTIPLIER * 50; i++) {
+    var author = users[Math.floor(Math.random() * users.length)]
+    var post = new Post({
+        title: randomTitle('books'),
+        body: randomTitle('books'),
+        author: author._id,
+    });
+        posts.push(post);
     }
-
-
-    // Seed other models...
 
 
     // ----------------------------------------
     // Finish
     // ----------------------------------------
     console.log('Saving...');
+
+
     var promises = [];
     [
         users,
+        posts
         // Other models...
     ].forEach((collection) => {
         collection.forEach((model) => {
