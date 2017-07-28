b975a733175d44170d9f46001f886575f6c5b50b
Christian Florez <florez.christian@gmail.com>
2017-07-28 01:02:47 -0400

    fix post show bug

--- models/comment.js
+++ models/comment.js
@@ -24,14 +24,12 @@ const CommentSchema = new Schema({
   timestamps: true
 });
 
-var autoPopulateChildren = function(next) {
-    this.populate('children');
-    next();
-};
+// var autoPopulateChildren = function(next) {
+//     this.populate('children');
+//     next();
+// };
 
-CommentSchema
-.pre('findOne', autoPopulateChildren)
-.pre('find', autoPopulateChildren)
+// CommentSchema.pre('findOne', autoPopulateChildren).pre('find', autoPopulateChildren)
 
 const Comment = mongoose.model('Comment', CommentSchema);
 
