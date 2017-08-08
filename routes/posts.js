882a79eddb6670f970715f426e5d0f14880a15d4
Greg Filipczak <gregfilipczak@gmail.com>
2017-08-08 21:31:05 +0000

    recursion

--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,7 @@ var { Post } = models;
 
 router.get("/posts", (req, res) => {
   Post.find()
-    .populate("users", "username")
+    .populate("user", "username")
     .then(posts => {
       console.log(posts);
       res.render("posts/index", { posts });
@@ -16,12 +16,39 @@ router.get("/posts", (req, res) => {
 router.get("/posts/:id", (req, res) => {
   let id = req.params.id;
   Post.findById(id)
-    .populate("comments score")
+    .populate([
+      "user",
+      "score",
+      {
+        path: "comments",
+        populate: ["user", "score"]
+      }
+    ])
     .then(post => {
       console.log(post);
       res.render("posts/show", { post });
     })
     .catch(e => res.status(500).send(e.stack));
 });
+
+let promiseArray = [];
+
+function gatherNestedComments(comments) {
+  for (let i = 0; i < comments.length; i++) {
+    // gatherNestedComments(comment)
+    if (comments[i].comments.length > 0) {
+      comments[i].comments.map(comment => {
+        let p = Comment.findById(comment);
+        promiseArray.push(p);
+        return p;
+      });
+      comments[i].comments = gatherNestedComments(comments[i].comments);
+    } else {
+      return comments[i].comments;
+    }
+  }
+}
+
+router.get("/posts/new", (req, res) => {});
 
 module.exports = router;
