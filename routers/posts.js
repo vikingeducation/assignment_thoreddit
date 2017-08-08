965759d743c672198a7257d7234ad5d3fc1fc136
EdTriplett <EdTriplett@users.noreply.github.com>
2017-08-08 18:02:23 -0400

    todo: new comment user

--- routers/posts.js
+++ routers/posts.js
@@ -18,10 +18,24 @@ router.get("/show/:id", (req, res) => {
   Posts.findById(req.params.id)
     .populate("score")
     .populate("user")
-    .populate("comments")
+    .populate({path:"comments", populate: [{path:'user'}, {path:'score'}]})
     .then(post => {
       res.render("posts/show", { post });
     });
 });
+
+router.get("/:id/comments/new", (req, res) =>{
+  res.render('comments/new')
+})
+
+router.post("/:id/comments/new", (req, res) =>{
+  let commentParams = {
+    kind: "comments",
+    text: req.body.comment.text,
+    user: 
+  }
+  Scorables.insert()
+})
+
 
 module.exports = router;
