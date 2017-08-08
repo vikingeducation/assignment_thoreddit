0a534301d426d9282fab228c61c4ca9ff4fd1c88
Will Timpson <will.timpson@gmail.com>
2017-08-08 16:16:47 -0400

    even more views!!

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,5 +5,11 @@ module.exports = {
     return User.find({}, {}, { sort: { createdAt: -1 } }).populate(
       "comment post"
     );
+  },
+  new: params => {
+    return User.create({ username: params.username, email: params.email });
+  },
+  getById: id => {
+    return User.findById(id).populate("comment post");
   }
 };
--- routers/users.js
+++ routers/users.js
@@ -1,12 +1,31 @@
 const router = require("express").Router();
 const { UserController } = require("../controllers");
+const h = require("../helpers");
 
 router.get("/", (req, res) => {
   UserController.getAll()
-    .then(users => {
-      res.render("users/index", { users });
-    })
+    .then(users => res.render("users/index", { users }))
     .catch(e => res.status(500).send(e.stack));
+});
+
+router.get("/new", (req, res) => {
+  res.render("users/new");
+});
+
+router.post("/new", (req, res) => {
+  UserController.new(req.body)
+    .then(user => res.redirect(h.userPath(user._id)))
+    .catch(e => res.status(500).send(e.stack));
+});
+
+router.get("/:id", (req, res) => {
+  UserController.getById(req.params.id).then(user => {
+    if (user) res.render("users/single", { user });
+    else {
+      req.flash("alert", "User not found");
+      res.redirect(h.usersPath());
+    }
+  });
 });
 
 module.exports = router;
