642a9f29f88ff52f20708990281cef04c1088f91
Eric Glover <etgt28@mail.missouri.edu>
2017-08-08 15:16:50 -0500

    users front-end resource

--- app.js
+++ app.js
@@ -2,33 +2,45 @@ const express = require("express");
 const app = express();
 
 // Setting up Body Parser
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 // Setting up Cookie session
-var cookieSession = require('cookie-session');
-app.use(cookieSession({
-  name: 'session',
-  keys: ['asdf1234567890qwer']
-}));
+var cookieSession = require("cookie-session");
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["asdf1234567890qwer"]
+  })
+);
 
+//METHOD OVERRIDING
+const methodOverride = require("method-override");
+const getPostSupport = require("express-method-override-get-post-support");
+
+app.use(
+  methodOverride(
+    getPostSupport.callback,
+    getPostSupport.options // { methods: ['POST', 'GET'] }
+  )
+);
+//METHOD OVERRIDING
+
+//sessions?
 app.use((req, res, next) => {
   res.locals.session = req.session;
   res.locals.currentUser = req.session.currentUser;
   next();
 });
 
-
 // Setting up Handlebars
-const expressHandlebars = require('express-handlebars');
+const expressHandlebars = require("express-handlebars");
 const hbs = expressHandlebars.create({
-  partialsDir: 'views/',
-  defaultLayout: 'main'
+  partialsDir: "views/",
+  defaultLayout: "main"
 });
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 //start the mongoose connection
 const mongoose = require("mongoose");
@@ -40,14 +52,18 @@ app.use((req, res, next) => {
   }
 });
 
+//later figure out the main page
+app.get("/", (req, res) => {
+  res.redirect("/users");
+});
 
 // Import our routers
 const usersRoutes = require("./routers/users");
-app.use('/', usersRoutes);
+app.use("/users", usersRoutes);
 
 const port = process.env.PORT || 3000;
 const host = "localhost";
 
 app.listen(port, host, () => {
   console.log("I'm listening here");
-})
+});
