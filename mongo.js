7fe3649d75218be02a1748773f37949859184abb
Eric Glover <etgt28@mail.missouri.edu>
2017-08-08 11:45:49 -0500

    changes?

--- mongo.js
+++ mongo.js
@@ -7,5 +7,6 @@ module.exports = () => {
   var envUrl = process.env[config.use_env_variable];
   var localUrl = `mongodb://${config.host}/${config.database}`;
   var mongoUrl = envUrl ? envUrl : localUrl;
+  console.log(`mongoUrl = ${mongoUrl}`);
   return mongoose.connect(mongoUrl);
 };
